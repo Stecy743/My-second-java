3c7cb7fd415f77731f632d69a33a48bdc00419f2
Stecy743 <135418950+Stecy743@users.noreply.github.com>
2024-07-09 17:34:48 +0300

    Add files via upload

---
+++ Java Full/Main.java
@@ -0,0 +1,27 @@
+public class Main{
+    
+    public static void main(String[] args) {
+
+        
+       
+        int x= 123;// declaration
+        x=123;//assignment
+        int x=; //intialization
+        double y =3.14;
+        boolean z= false;
+        char symbol='@';
+        String name="Stecy";
+
+        System.out.println("Hello" +name);
+    }
+}
+
+   
+        
+        
+
+      
+    
+
+
+
